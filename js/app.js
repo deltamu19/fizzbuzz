1b8baab3188d6ee51cf96ae80e966efe32063edc
deltamu19 <daryll.martinez@gmail.com>
2016-06-27 22:04:21 -0700

    refactor edits 1

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
-for (var num = 1; num <= 100; num++) {
-  if (num % 15 === 0 ) {
-    console.log('FizzBuzz')
-  }
-  else if (num % 3 === 0) {
-    console.log('Fizz')
-  }
-  else if (num % 5 === 0 ) {
-    console.log('Buzz')
-  }
-  else {
-  	console.log(num)
-  }
 
-}
+$(document).ready(function() {
+  
+  $('#input').submit(function() {
+  
+  var num = parseInt('#input');
+  
+  var fbGame = function(num) {
+    for (var num = 1; num <= 100; num++) {
+    if (num % 15 === 0 ) {
+      console.log('FizzBuzz')
+    }
+    else if (num % 3 === 0) {
+      console.log('Fizz')
+    }
+    else if (num % 5 === 0 ) {
+      console.log('Buzz')
+    }
+    else {
+      console.log(num)
+    }
+    }
+  }
+  })
+})
